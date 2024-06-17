41ccc539d153af4e975f72c909672b7dc3d18791
Huy Nguyen <huy55465@gmail.com>
2024-06-17 10:20:52 -0400

    feat: customize the dashboard look a bit
    commit-by: Huy Nguyen

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,1 +1,18 @@
-require("dashboard").setup({})
+require("dashboard").setup({
+    config = {
+        shortcut = {
+            { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+            {
+                icon = " ",
+                icon_hl = "@variable",
+                desc = "Files",
+                group = "Label",
+                action = "Telescope find_files",
+                key = "f",
+            },
+        },
+        footer = {
+            "✨ Another day on Neovim ✨",
+        },
+    },
+})
