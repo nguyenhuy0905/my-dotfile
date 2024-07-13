b365d55dcb6a381e9d4061c2dfa356f4b6255a20
Huy Nguyen <huy55465@gmail.com>
2024-07-13 01:19:55 -0400

    fix: tab size 2 and shiftwidth size 2 also
    You can change these with:
    :set tabstop=4
    :set shiftwidth=4
    
    commit-by: Huy Nguyen

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -2,14 +2,14 @@ local CMD = vim.cmd
 -- i like 4-tabs
 CMD("set expandtab")
 CMD("tnoremap <C-n> <C-\\><C-n>")
-CMD("set tabstop=4")
-CMD("set softtabstop=4")
-CMD("set shiftwidth=4")
+CMD("set tabstop=2")
+CMD("set softtabstop=2")
+CMD("set shiftwidth=2")
 CMD("set number")
 CMD("set relativenumber")
 -- color
 CMD("set termguicolors")
-CMD.colorscheme("tokyonight-storm")
+CMD.colorscheme("catppuccin")
 -- treesitter
 CMD("set foldmethod=expr")
 CMD("set foldexpr=nvim_treesitter#foldexpr()")
