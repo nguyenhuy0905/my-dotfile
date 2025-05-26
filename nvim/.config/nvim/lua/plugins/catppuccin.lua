fc14ed180dd89cbe469440e8e91bb911f6ab8088
Huy Nguyen <huy55465@gmail.com>
2025-05-26 14:52:36 -0400

    feat(nvim): use catppuccin frappe
    I like the lighter shade.
    
    Commit-by: Huy Nguyen

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,1 +1,10 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+        flavour = "frappe",
+        show_end_of_buffer = true,
+        term_colors = true,
+    },
+}
