06d753bca9717a79582aa16c42b15e7f1e5053b0
Huy Nguyen <huy55465@gmail.com>
2025-05-27 10:16:30 -0400

    feat(nvim): use lazydev
    I'm getting lazy.
    
    Commit-by: Huy Nguyen

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -3,6 +3,19 @@ return {
     event = "InsertEnter",
     opts = {
         signature = { enabled = true },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            per_filetype = {
+                lua = { inherit_defaults = true, "lazydev" },
+            },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100,
+                },
+            },
+        },
     },
     version = "1.*",
 }
