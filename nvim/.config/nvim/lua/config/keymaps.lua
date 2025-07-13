9a7be981ebb3aafb75cbe3c53605454f9037d8d4
Huy Nguyen <huy55465@gmail.com>
2025-07-13 14:07:03 -0400

    feat(nvim): add move selection up/down keymap
    alt+j to move selection up 1 line.
    alt+k to move selection down 1 line.
    
    Signed-off-by: Huy Nguyen <huy55465@gmail.com>

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set(
     "<Esc>?\\%V",
     { desc = "Search in selected area (backwards)" }
 )
+vim.keymap.set("x", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down 1 line" })
+vim.keymap.set("x", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up 1 line" })
