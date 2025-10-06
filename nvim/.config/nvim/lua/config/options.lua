f21878c33aef693c343e2165e9e0770c78b53b41
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-10-06 11:59:51 -0400

    wip(nvim): use default autocomplete instead
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,32 @@ vim.opt.relativenumber = true
 vim.lsp.set_log_level("off")
 vim.opt.shell = "fish"
 vim.o.history = 250
+
+if not vim.o.completeopt:find("fuzzy") then
+    vim.o.completeopt = vim.o.completeopt .. ",fuzzy"
+end
+
+if not vim.o.completeopt:find("noselect") then
+    vim.o.completeopt = vim.o.completeopt .. ",noselect"
+end
+
+if not vim.o.completeopt:find("preview") then
+    vim.o.completeopt = vim.o.completeopt .. ",preview"
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if not client then
+            return
+        end
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(
+                true,
+                client.id,
+                ev.buf,
+                { autotrigger = true }
+            )
+        end
+    end,
+})
