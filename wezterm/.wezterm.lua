b886cb809d25440777b74007fc0828c856ccf801
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-11 13:01:03 -0500

    feat: wezterm!!
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require('wezterm')
+local font = wezterm.font('Iosevka Nerd Font')
+
+return {
+    font_size = 24,
+    font = font,
+    color_scheme = 'Catppuccin Mocha',
+    use_fancy_tab_bar = false,
+    colors = {
+        tab_bar = {
+            background = '#1e1e2e',
+        },
+    },
+    default_prog = { '/usr/bin/fish' },
+}
