21ddacf3c01fbbde452517d286bf844c256db392
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-11 13:01:21 -0500

    feat(nvim): lsp!!
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,8 +4,8 @@ require("config.netrw")
 require("config.keymaps")
 -- config.options required in config.lazy already
 
--- vim.lsp.enable({
---     "lua_ls",
---     "clangd",
---     "rust_analyzer",
--- })
+vim.lsp.enable({
+    "lua_ls",
+    "clangd",
+    "rust_analyzer",
+})
