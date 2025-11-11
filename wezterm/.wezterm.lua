9cb850f1faba588bfa66ceda5581df5bbc53e273
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-11 13:30:26 -0500

    fix: text scaling
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local font = wezterm.font('Iosevka Nerd Font')
 
 return {
-    font_size = 24,
+    font_size = 14,
     font = font,
     color_scheme = 'Catppuccin Mocha',
     use_fancy_tab_bar = false,
