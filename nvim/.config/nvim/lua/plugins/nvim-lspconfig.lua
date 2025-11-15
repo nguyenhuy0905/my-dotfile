2ba1bd6eee49fbf8bf91ea76e2fcd79dbc10fc17
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-15 11:32:20 -0500

    feat(nvim): config clangd a bit more
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,17 @@
 return {
     "neovim/nvim-lspconfig",
     event = "BufEnter",
+    config = function ()
+        vim.lsp.config('clangd', {
+            cmd = {
+                'clangd',
+                '--clang-tidy',
+                '-j=8',
+                '--malloc-trim',
+                '--background-index',
+                '--pch-storage=memory',
+            }
+        })
+    end,
     version = "*",
 }
