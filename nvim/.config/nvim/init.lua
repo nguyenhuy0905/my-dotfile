c17fcb39fc3a281ca512bafed13b41f3b075c0ce
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-11-15 11:31:57 -0500

    fix(nvim): re-enable lsp, add statusline config
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,10 +2,11 @@ require("config.lazy")
 require("config.colorscheme")
 require("config.netrw")
 require("config.keymaps")
+require("config.statusline")
 -- config.options required in config.lazy already
 
--- vim.lsp.enable({
---     "lua_ls",
---     "clangd",
---     "rust_analyzer",
--- })
+vim.lsp.enable({
+    "lua_ls",
+    "clangd",
+    "rust_analyzer",
+})
